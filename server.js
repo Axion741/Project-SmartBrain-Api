0e7c644afaf5b1f4bf2e4ff4896a6758d2c1d6fb
Axion741 <k.ballagher@gmail.com>
2018-11-20 12:50:01 +0000

    Setup Heroku Database

--- server.js
+++ server.js
@@ -14,10 +14,8 @@ const profile = require('./controllers/profile')
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'Infinity011093',
-      database : 'smartbrain'
+      host : 'process.env.DATABASE_URL',
+      ssl:true
     }
   });
 
